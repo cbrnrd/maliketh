8db15809794a54673b0341a6c9ca9626aa54a74c
Richard Li <jsksosdgkd29y@gmail.com>
2023-04-18 18:54:32 -0400

    don't look at the last commit's IP

--- implant/include/constants.h
+++ implant/include/constants.h
@@ -6,7 +6,7 @@
 #endif
 
 #define CONTENT_TYPE_JSON L"Content-Type: application/json"
-#define C2_URL L"10.110.188.49"
+#define C2_URL L"localhost"
 #define TASK_RESULTS_ENDPOINT L"/c2/task"
 
 #endif // CONSTANTS_H_
