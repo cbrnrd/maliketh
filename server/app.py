4fa5a3290a3220189dbe4f728bcaecaf240d8e2b
Carter Brainerd <0xCB@protonmail.com>
2023-04-18 18:57:41 -0400

    Remove old files

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import gunicorn.app.base
 from maliketh.buildapp import build_operator_app, build_c2_app, init_db
 from maliketh.logging.standard_logger import StandardLogger, LogLevel
 from maliketh.operator.rmq import rmq_setup
@@ -12,6 +13,21 @@ from maliketh.config import set_c2_profile, DEFAULT_C2_PROFILE
 operator_app = build_operator_app()
 c2_app = build_c2_app()
 
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 def parse_options():
     opts = {}
@@ -127,10 +143,10 @@ def main():
 
   if opts.start_operator:
     logger.info("Starting operator listener on %s:%s" % (opts.address, opts.port))
-    operator_app.run(host=opts.address, port=opts.port, debug=opts.debug)
+    StandaloneApplication(operator_app, {}).run(host=opts.address, port=opts.port, debug=opts.debug)
   elif opts.start_c2:
     logger.info("Starting C2 listener on %s:%s" % (opts.c2_address, opts.c2_port))
-    c2_app.run(host=opts.c2_address, port=opts.c2_port, debug=opts.debug)
+    StandaloneApplication(c2_app, {}).run(host=opts.c2_address, port=opts.c2_port, debug=opts.debug)
 
 
 if __name__ == '__main__':
